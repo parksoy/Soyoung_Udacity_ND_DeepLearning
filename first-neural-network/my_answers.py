44ce5aa8c17a7e0d64fed42aad8bf56ed1c5f681
parksoy <parksoy@users.noreply.github.com>
2018-05-02 06:30:49 -0700

    implemented forward pass and backpropagation, IP on run

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -64,12 +64,13 @@ class NeuralNetwork(object):
         #### Implement the forward pass here ####
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = sigmoid(hidden_input) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = hidden_outputs # signals into final output layer
+        final_outputs = sigmoid(np.dot(final_inputs,
+                                self.weights_hidden_to_output)) # signals from final output layer
 
         return final_outputs, hidden_outputs
 
@@ -88,20 +89,22 @@ class NeuralNetwork(object):
         ### Backward pass ###
 
         # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
 
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
+        output_error_term = error * final_outputs * (1 - final_outputs)
+        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output)
 
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
 
-        hidden_error_term = None
+
+        hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
+
 
         # Weight step (input to hidden)
-        delta_weights_i_h += None
+        delta_weights_i_h += hidden_error_term * X[:, None]
         # Weight step (hidden to output)
-        delta_weights_h_o += None
+        delta_weights_h_o += output_error_term * hidden_outputs
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -114,8 +117,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += None # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += None # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += learning_rate * delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += learning_rate * delta_weights_i_h / n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features
@@ -127,12 +130,14 @@ class NeuralNetwork(object):
 
         #### Implement the forward pass here ####
         # TODO: Hidden layer - replace these values with the appropriate calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(x, weights_input_hidden) # signals into hidden layer
+        hidden_outputs = sigmoid(hidden_input) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with the appropriate calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = hidden_outputs # signals into final output layer
+        final_outputs = sigmoid(np.dot(hidden_output,weights_hidden_output))
+
+        # signals from final output layer
 
         return final_outputs
 
